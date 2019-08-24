5b28d7882e332c07a129eb962afe6947448ecfc1
assm <assm14401@gmail.com>
2019-08-23 23:15:36 -0500

    Finished Algorithm

--- bubblefunc.cpp
+++ bubblefunc.cpp
@@ -1,5 +1,6 @@
 /*------DECLARATIONS------*/
 #include <iostream>
+#define _tam_ 5
 using namespace std;
 
 /*_bubblesort_ con punteros a funcion*/
@@ -14,18 +15,58 @@ void * _bubblesort_(void * _array_, void (*_puntero_) (void *, int _i, int _k)){
 /*------FUNCTIONS------*/
 
 void  _b_int_ (void * _vector_, int _i,int _k){
+  int tempo;
+  int *temp = (int*)_vector_;
+  if(temp[_i] > temp[_i + 1])
+  {
+    tempo = temp[_i];
+    temp[_i] = temp[_i+1];
+    temp[_i+1] = tempo;
+  }
 }
 
 void  _b_float_ (void * _vector_, int _i,int _k){
+  float tempo;
+  float *temp = (float*)_vector_;
+  if(temp[_i] > temp[_i + 1])
+  {
+    tempo = temp[_i];
+    temp[_i] = temp[_i+1];
+    temp[_i+1] = tempo;
+  }
 }
 
 void  _b_double_ (void * _vector_, int _i,int _k){
+  double tempo;
+  double *temp = (double*)_vector_;
+  if(temp[_i] > temp[_i + 1])
+  {
+    tempo = temp[_i];
+    temp[_i] = temp[_i+1];
+    temp[_i+1] = tempo;
+  }
 }
 
 void  _b_char_ (void * _vector_, int _i,int _k){
+  char tempo;
+  char *temp = (char*)_vector_;
+  if(temp[_i] > temp[_i + 1])
+  {
+    tempo = temp[_i];
+    temp[_i] = temp[_i+1];
+    temp[_i+1] = tempo;
+  }
 }
 
-
+template<typename T>
+void printarr(T* arr, int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
 
 /*------MAIN------*/
 int main(){
@@ -39,6 +80,11 @@ int main(){
 	_bubblesort_(_array_2,_b_float_);
 	_bubblesort_(_array_3,_b_double_);
 	_bubblesort_(_array_4,_b_char_);
-    return 0;
+
+  printarr(_array_1, _tam_);
+  printarr(_array_2, _tam_);
+  printarr(_array_3, _tam_);
+  printarr(_array_4, _tam_);
+  return 0;
 }
 
